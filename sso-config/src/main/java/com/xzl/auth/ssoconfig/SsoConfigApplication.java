cbb2a26dee4ccc6956bd3a85aa0f0f820134f8d7
cjh <1085549402@qq.com>
2018-03-12 23:43:37 +0800

    添加了spring cloud 做配置

---
+++ sso-config/src/main/java/com/xzl/auth/ssoconfig/SsoConfigApplication.java
@@ -0,0 +1,12 @@
+package com.xzl.auth.ssoconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsoConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsoConfigApplication.class, args);
+	}
+}
