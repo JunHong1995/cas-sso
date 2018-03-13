e9240474d81b6cd3d911420b6fa259bd7d638062
JunHongChen <chenjunhong@zailingtech.com>
2018-03-13 15:19:44 +0800

    添加cas-rest-client

---
+++ cas-rest-client/src/main/java/com/xzl/auth/sso/rest/client/RestClientApplication.java
@@ -0,0 +1,19 @@
+/*
+ * 版权所有.(c)2008-2017. 卡尔科技工作室
+ */
+
+package com.xzl.auth.sso.rest.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动程序
+ *
+ */
+@SpringBootApplication
+public class RestClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RestClientApplication.class, args);
+    }
+}
